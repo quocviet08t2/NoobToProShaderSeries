24d55a1c62c1274dd60800998c09fa6dd74fcba9
Viet Phan <phanhoquocviet@outlook.com>
2016-05-23 22:43:15 +0700

    Revert "- Remove files."
    This reverts commit 36c0683e27cb5a1868f41a58141a480d2b913e86.

---
+++ Assets/xxxCube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class xxxCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        float xxx = 2;
+
+        float y = 3;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
