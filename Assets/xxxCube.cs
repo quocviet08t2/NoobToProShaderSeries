417f790a23f8b5911e34ec2d8699d4b57d15829f
Viet Phan <quocviet.08t2.ah@gmail.com>
2016-05-23 11:18:40 +0700

    - Add prefab cubexxx.

---
+++ Assets/xxxCube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class xxxCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        float xxx = 2;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
