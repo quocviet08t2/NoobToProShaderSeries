aa2c32cd9a252ca22aa7e3211053289b3bb9854f
Viet Phan <quocviet.08t2.ah@gmail.com>
2016-05-29 09:21:25 +0700

    - Restruct folder Editor.

---
+++ Assets/Scripts/UnitySerialized/Editor/ColorPointDrawer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace UnitySerialized
+{
+    [CustomPropertyDrawer(typeof(ColorPoint))]
+    public class ColorPointDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return label != GUIContent.none && Screen.width < 333 ? (16f + 18f) : 16f;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            int oldIndentLevel = EditorGUI.indentLevel;
+            label = EditorGUI.BeginProperty(position, label, property);
+            Rect contentPos = EditorGUI.PrefixLabel(position, label);
+
+            if (contentPos.height > 16f)
+            {
+                position.height = 16f;
+
+                // Change indent level and indented this
+                EditorGUI.indentLevel += 1;
+                contentPos = EditorGUI.IndentedRect(position);
+
+                contentPos.y += 18f;
+            }
+
+            contentPos.width *= 0.75f;
+            EditorGUI.indentLevel = 0;
+            EditorGUI.PropertyField(contentPos, property.FindPropertyRelative("position"), GUIContent.none);
+            contentPos.x += contentPos.width;
+            contentPos.width /= 3f;
+            EditorGUIUtility.labelWidth = 14f;
+            EditorGUI.PropertyField(contentPos, property.FindPropertyRelative("color"), new GUIContent("C"));
+            EditorGUI.EndProperty();
+            EditorGUI.indentLevel = oldIndentLevel;
+        }
+    }
+}
---
+++ Assets/Scripts/UnitySerialized/Editor/SignUpWindow.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class SignupWindow : EditorWindow
+{
+
+	GUITextField username;
+	GUITextField realname;
+	GUIButton registerButton;
+
+	// Optional, but may be convenient.
+	List<IGUI> gui = new List<IGUI>();
+
+	[MenuItem("Example/Show Window")]
+	public static void ShowWindow()
+	{
+		GetWindow<SignupWindow>().Show();
+	}
+
+	void OnEnable()
+	{
+
+		username = new GUITextField();
+		username.label.text = "Username";
+		username.text = "JDoe";
+
+		realname = new GUITextField();
+		realname.label.text = "Real name";
+		realname.text = "John Doe";
+
+		registerButton = new GUIButton();
+		registerButton.label.text = "Register";
+		registerButton.Clicked += RegisterUser;
+
+		gui.Add(username);
+		gui.Add(realname);
+		gui.Add(registerButton);
+	}
+
+	void RegisterUser()
+	{
+		var msg = "Registering " + realname.text + " as " + username.text;
+		Debug.Log(msg);
+	}
+
+	void OnGUI()
+	{
+		foreach (var item in gui)
+			item.OnGUI();
+	}
+}
+
+public interface IGUI
+{
+	void OnGUI();
+}
+
+public class GUITextField : IGUI
+{
+	public string text = "";
+	public GUIContent label = new GUIContent();
+
+	// Unused in my example, but you may want to check if
+	// a textbox becomes empty for example.
+	public event System.Action<string> TextChanged;
+
+	public void OnGUI()
+	{
+		// Also I wanted to show you BeginChangeCheck and EndChangeCheck
+		// which is the Unity GUI way of checking if a GUI control changed...
+		EditorGUI.BeginChangeCheck();
+		text = EditorGUILayout.TextField(label, text);
+		if (EditorGUI.EndChangeCheck() && TextChanged != null)
+			TextChanged(text);
+	}
+}
+
+public class GUIButton : IGUI
+{
+	public GUIContent label = new GUIContent();
+	public event System.Action Clicked;
+
+	public void OnGUI()
+	{
+		if (GUILayout.Button(label) && Clicked != null)
+			Clicked();
+	}
+}
